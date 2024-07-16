0f015c1e94e29eab8110c96abe37a1d949c4c022
m-akira0924 <mizushima.akira@i3design.co.jp>
2024-07-16 16:52:37 +0900

    ValueObjectとしてBookIdクラスを作成

---
+++ apps/backend/app/Domain/ValueObjects/Book/BookId.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Domain\ValueObjects;
+
+class BookId
+{
+    private string $id;
+
+    public function  __construct(string $id)
+    {
+        if(empty($id)) {
+            throw new \InvalidArgumentException('Book ID cannot be empty');
+        }   $this->id = $id;
+    }
+
+    public function getValue(): string
+    {
+        return $this->id;
+    }
+
+    public function equals(BookId $other): bool
+    {
+        return $this->id === $other->getValue();
+    }
+}
