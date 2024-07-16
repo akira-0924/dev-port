4afdf772145e657a0a01f0cb762a8847cd11c871
m-akira0924 <mizushima.akira@i3design.co.jp>
2024-07-16 16:48:41 +0900

    モデル、エンティティクラス実装

---
+++ apps/backend/app/Domain/Entities/Book/Book.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Domain\Entitiees\Book;
+
+class Book
+{
+    private string $id;
+    private string $title;
+    private string $author;
+    private ?string $description;
+
+    private function __construct(string $id, string $title, string $author, string $description)
+    {
+        $this->id = $id;
+        $this->title = $title;
+        $this->author = $author;
+        $this->description = $description;
+    }
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function getAuthor(): string
+    {
+        return $this->author;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+}
